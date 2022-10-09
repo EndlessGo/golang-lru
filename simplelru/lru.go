2b654ac6f26bca6b111152e6ad0b86e570bf1aed
wangchao <wangchao@ka60om.com>
2022-10-09 19:04:16 +0800

    add: simplelru

--- simplelru/lru.go
+++ simplelru/lru.go
@@ -9,20 +9,27 @@ import (
 type EvictCallback func(key interface{}, value interface{})
 
 // LRU implements a non-thread safe fixed size LRU cache
+// LRU缓存数据结构，特点：非线程安全，固定大小，并且没有时间戳
 type LRU struct {
-	size      int
-	evictList *list.List
-	items     map[interface{}]*list.Element
-	onEvict   EvictCallback
+	// 大小
+	size int
+	// 用双向链表存储元素
+	evictList *list.List // evict 淘汰
+	// 元素查找表，查某个元素在链表中的位置
+	items map[interface{}]*list.Element
+	// 清除元素时的回调
+	onEvict EvictCallback
 }
 
 // entry is used to hold a value in the evictList
+// 链表元素，实际存储的数据结构
 type entry struct {
 	key   interface{}
 	value interface{}
 }
 
 // NewLRU constructs an LRU of the given size
+// 创建指定大小的LRU
 func NewLRU(size int, onEvict EvictCallback) (*LRU, error) {
 	if size <= 0 {
 		return nil, errors.New("must provide a positive size")
@@ -37,6 +44,7 @@ func NewLRU(size int, onEvict EvictCallback) (*LRU, error) {
 }
 
 // Purge is used to completely clear the cache.
+// 清空缓存
 func (c *LRU) Purge() {
 	for k, v := range c.items {
 		if c.onEvict != nil {
@@ -48,15 +56,18 @@ func (c *LRU) Purge() {
 }
 
 // Add adds a value to the cache.  Returns true if an eviction occurred.
+// 插入
 func (c *LRU) Add(key, value interface{}) (evicted bool) {
 	// Check for existing item
 	if ent, ok := c.items[key]; ok {
+		// 更新缓存：如果缓存命中，将其移至链表头，并更新元素数据
 		c.evictList.MoveToFront(ent)
 		ent.Value.(*entry).value = value
 		return false
 	}
 
 	// Add new item
+	// 若没有命中缓存，则插入新元素，并记录位置
 	ent := &entry{key, value}
 	entry := c.evictList.PushFront(ent)
 	c.items[key] = entry
@@ -64,12 +75,14 @@ func (c *LRU) Add(key, value interface{}) (evicted bool) {
 	evict := c.evictList.Len() > c.size
 	// Verify size not exceeded
 	if evict {
+		// 若插入新元素后缓存长度溢出，则删除最早的元素
 		c.removeOldest()
 	}
 	return evict
 }
 
 // Get looks up a key's value from the cache.
+// 查找元素，若缓存命中则更新最近访问（将其移至链表头）
 func (c *LRU) Get(key interface{}) (value interface{}, ok bool) {
 	if ent, ok := c.items[key]; ok {
 		c.evictList.MoveToFront(ent)
@@ -83,6 +96,7 @@ func (c *LRU) Get(key interface{}) (value interface{}, ok bool) {
 
 // Contains checks if a key is in the cache, without updating the recent-ness
 // or deleting it for being stale.
+// 是否包含元素，不会更新
 func (c *LRU) Contains(key interface{}) (ok bool) {
 	_, ok = c.items[key]
 	return ok
@@ -90,6 +104,7 @@ func (c *LRU) Contains(key interface{}) (ok bool) {
 
 // Peek returns the key value (or undefined if not found) without updating
 // the "recently used"-ness of the key.
+// 查找元素，不会更新最近访问（将其移至链表头）
 func (c *LRU) Peek(key interface{}) (value interface{}, ok bool) {
 	var ent *list.Element
 	if ent, ok = c.items[key]; ok {
@@ -100,6 +115,7 @@ func (c *LRU) Peek(key interface{}) (value interface{}, ok bool) {
 
 // Remove removes the provided key from the cache, returning if the
 // key was contained.
+// 删除元素
 func (c *LRU) Remove(key interface{}) (present bool) {
 	if ent, ok := c.items[key]; ok {
 		c.removeElement(ent)
@@ -109,6 +125,7 @@ func (c *LRU) Remove(key interface{}) (present bool) {
 }
 
 // RemoveOldest removes the oldest item from the cache.
+// 删除最久未访问的元素
 func (c *LRU) RemoveOldest() (key, value interface{}, ok bool) {
 	ent := c.evictList.Back()
 	if ent != nil {
@@ -120,6 +137,7 @@ func (c *LRU) RemoveOldest() (key, value interface{}, ok bool) {
 }
 
 // GetOldest returns the oldest entry
+// 获取最久未访问的元素，不会更新最近访问
 func (c *LRU) GetOldest() (key, value interface{}, ok bool) {
 	ent := c.evictList.Back()
 	if ent != nil {
@@ -130,6 +148,7 @@ func (c *LRU) GetOldest() (key, value interface{}, ok bool) {
 }
 
 // Keys returns a slice of the keys in the cache, from oldest to newest.
+// 获取所有元素的键，从最久到最近
 func (c *LRU) Keys() []interface{} {
 	keys := make([]interface{}, len(c.items))
 	i := 0
@@ -146,12 +165,14 @@ func (c *LRU) Len() int {
 }
 
 // Resize changes the cache size.
+// 更改大小
 func (c *LRU) Resize(size int) (evicted int) {
 	diff := c.Len() - size
 	if diff < 0 {
 		diff = 0
 	}
 	for i := 0; i < diff; i++ {
+		// 如果缩小，则删除对应数量的最久元素
 		c.removeOldest()
 	}
 	c.size = size
@@ -159,6 +180,7 @@ func (c *LRU) Resize(size int) (evicted int) {
 }
 
 // removeOldest removes the oldest item from the cache.
+// 删除最久未被访问的元素
 func (c *LRU) removeOldest() {
 	ent := c.evictList.Back()
 	if ent != nil {
@@ -167,6 +189,7 @@ func (c *LRU) removeOldest() {
 }
 
 // removeElement is used to remove a given list element from the cache
+// 删除指定元素
 func (c *LRU) removeElement(e *list.Element) {
 	c.evictList.Remove(e)
 	kv := e.Value.(*entry)
